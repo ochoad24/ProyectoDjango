66eef9a568ad85fb4467c9ff873590865bab2cd1
Daniel Ochoa <ochoad24@gmail.com>
2019-11-02 18:53:42 -0600

    Inicio del proyecto

---
+++ pos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+class Categoria(models.Model):
+    nombre=models.CharField(max_length=200)
+    created_date=models.DateTimeField(default=timezone.now)
+
+def __str__(self):
+    return self.nombre
