689705ade89c60c6f48235c2c05ac1b3ccb95baa
Daniel Ochoa <ochoad24@gmail.com>
2019-11-03 02:10:51 -0600

    Añadiendo diseño

--- pos/views.py
+++ pos/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Categoria
 # Create your views here.
+def categoria_list(request):
+    categorias=Categoria.objects.filter()
+    return render(request,'pos/categoria_list.html',{'categorias':categorias})
